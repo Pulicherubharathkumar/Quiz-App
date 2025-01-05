448b38007dbb7ea785a889c2170000d26a445748
PULICHERU BHARATH KUMAR <144251614+Pulicherubharathkumar@users.noreply.github.com>
2025-01-05 21:07:00 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,47 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Markup Language",
+      "High Tech Markup Language",
+      "Hyperlink Text Markup Language",
+      "Home Tool Markup Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: " What does CSS stand for?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Creative Style Sheets",
+      "Cascading Style Sheets",
+      "Computer Style Sheets",
+      "Custom Style Sheets"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the purpose of the script tag in HTML?",
+    answer: "To include external JavaScript code",
+    options: [
+      " To define the page’s structure",
+      "To include external CSS styles",
+      "To include external JavaScript code",
+      "To create hyperlinks"
+    ]
+  },
+    {
+    numb: 4,
+    question: "T Which of the following is the correct extension of the Python file?",
+    answer: ".py",
+    options: [
+      ".python",
+      " .pl",
+      ".py",
+      ".p"
+    ]
+  },
+    
+];
